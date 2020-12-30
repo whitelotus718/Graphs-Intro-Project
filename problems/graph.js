2450249847755eaeca875e1183c7fcf96f670b6c
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2020-12-30 19:29:29 +0100

    Got few specs working

--- problems/graph.js
+++ problems/graph.js
@@ -2,14 +2,26 @@
 class Graph {
   constructor() {
     // Code goes here ...
+    this.adjList = new Object();
+
   }
 
   addVertex(vertex) {
     // Code goes here ...
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = new Array();
+    }
+    if (this.adjList[vertex]) return;
   }
 
   addEdges(srcValue, destValue) {
     // Code goes here ...
+    if (!this.adjList[srcValue]) {
+      this.addVertex(srcValue)
+    } 
+    if (!this.adjList[destValue]) {
+      this.addVertex(destValue)
+    }
   }
 
   buildGraph(edges) {
